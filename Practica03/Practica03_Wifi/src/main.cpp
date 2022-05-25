08895b0e6019214b7d2d65cc6243571452cebed0
Rafa <100418630+Rfa-E@users.noreply.github.com>
2022-05-25 19:08:17 +0200

    Add files via upload

---
+++ Practica03/Practica03_Wifi/src/main.cpp
@@ -0,0 +1,49 @@
+#include <WiFi.h>
+#include <SPIFFS.h>
+#include <ESPAsyncWebServer.h>
+#include <Arduino.h>
+
+// SSID & Password
+const char* ssid = "MiFibra-2C06"; // Enter your SSID here
+const char* password = "UVtLn4Zh"; //Enter your Password here
+
+AsyncWebServer server(80); // Object of WebServer(HTTP port, 80 is defult)
+
+void setup(){
+  Serial.begin(115200);
+
+  if(!SPIFFS.begin())
+  {
+     Serial.println("An Error has occurred while mounting SPIFFS");
+     return;
+  }
+
+  WiFi.begin(ssid, password);
+ 
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting to WiFi..");
+  }
+  
+  Serial.println("");
+  Serial.println("WiFi connected successfully");
+  Serial.print("Got IP: ");
+  Serial.println(WiFi.localIP()); //Show ESP32 IP on serial
+  server.on("/Sharingan", HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    request->send(SPIFFS, "/Sharingan.html", "text/html");
+  });
+  server.on("/Sharingan.css", HTTP_GET, [](AsyncWebServerRequest *request)
+  {
+    request->send(SPIFFS, "/Sharingan.css", "text/css");
+  });
+
+  server.begin();
+  Serial.println("HTTP server started");
+  delay(100);
+}
+
+void loop() 
+{
+
+}
