5942a56f9de0da3144e174590858e06f56a1f77b
Rafa <100418630+Rfa-E@users.noreply.github.com>
2022-05-25 19:10:58 +0200

    Add files via upload

---
+++ Practica04/Practica04 Task1/src/main.cpp
@@ -0,0 +1,26 @@
+#include <Arduino.h>
+
+void anotherTask( void * parameter );
+
+void setup()
+{
+  Serial.begin(112500);
+  /* we create a new task here */
+  xTaskCreate(anotherTask,"another Task",10000,NULL,1,NULL); 
+}
+
+void loop()
+{
+  Serial.println("this is ESP32 Task");
+  delay(1000);
+}
+/* this function will be invoked when additionalTask was created */
+void anotherTask( void * parameter )
+{
+  /* loop forever */
+  for(;;)
+  {
+  Serial.println("this is another Task");
+  delay(1000);
+  }
+}
